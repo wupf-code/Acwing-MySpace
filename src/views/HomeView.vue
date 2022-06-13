872c0d48932caf946985dd3e7a07d0e6f94f32c4
wpf <2736292553@qq.com>
2022-06-13 20:03:50 +0800

    顶层设计

---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+<Content>
+   首页
+  </Content>
+</template>
+
+<script>
+import Content from "@/components/Content";
+
+export default {
+  name:'HomeView',
+  components:{
+    Content,
+  }
+}
+</script>
+
+
+
+<style scoped>
+
+</style>
---
+++ src/views/NotFound.vue
@@ -0,0 +1,19 @@
+<template>
+  <Content>
+    404
+  </Content>
+</template>
+
+<script>
+import Content from "@/components/Content";
+export default {
+  name: "NotFound",
+  components:{
+    Content,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
